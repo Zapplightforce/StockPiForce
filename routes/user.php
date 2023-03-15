f51e30ec76e4d1a0c52e207e2ef6ac6f40b1cb4f
Zapplightforce <pers0010@hz.nl>
2023-03-15 18:08:12 +0100

    new routes

---
+++ routes/user.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
+    Route::get('/news', [UserController::class, 'news'])->name('news');
+});
