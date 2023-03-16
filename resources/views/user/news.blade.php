2fee4f50181fd795a19a7f151007db706b7a366a
Zapplightforce <pers0010@hz.nl>
2023-03-16 22:45:46 +0100

    news site with iframe

--- resources/views/user/news.blade.php
+++ resources/views/user/news.blade.php
@@ -6,29 +6,65 @@
     </x-slot>
 
     <div class="py-8">
-        <div class="flex">
-            <div class="max-w-[400] mx-3">
-            <div class="bg-white dark:bg-gray-800 overflow-y-scroll shadow-sm sm:rounded-lg max-h-[650px]">
+        <div class="flex max-h-[calc(100vh-13rem)] mx-3 overflow-y-auto">
+            <div class="flex flex-col w-1/3 h-800px overflow-y-scroll">
 
-                    @foreach($articles as $article)
-                    <a href="{{$article->url}}">
-                    <div class="bg-gray-200 m-3 p-1 rounded-lg dark:bg-gray-600">
-                        <div class="mb-4">
-                            <h2 class="text-2xl font-bold dark:text-white">{{ $article->title }}</h2>
-                            <p class="text-gray-900">{{ $article->description }}</p>
+                @foreach($articles as $article)
+
+                    <a id="link" href="{{$article->url}}" class="article-link bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg max-w-sm mb-3">
+                        <div class="p-6">
+                            <h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                {{$article->title}}</h5>
+                            <p class="font-normal text-gray-700 dark:text-gray-400">{{$article->description}}</p>
                         </div>
-                    </div>
                     </a>
-                    @endforeach
-                </div>
-            </div>
 
-            <div class="mx-3 max-w-screen">
-                <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg max-h-[650px]">
-                    <iframe class="max-h-screen w-[1100px]"></iframe>
-                </div>
-            </div>
-
+                @endforeach
         </div>
+
+            <div class="flex mx-3 max-w-screen">
+                <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg max-h-[650px]">
+                    <iframe id="target-iframe" class="max-h-screen w-[1100px]"></iframe>
+                </div>
+            </div>
     </div>
+    </div>
+
+    <script>
+        document.querySelectorAll('.article-link').forEach(link => {
+            link.addEventListener('click', function(event) {
+                event.preventDefault();
+                const iframe = document.getElementById('target-iframe');
+                const url = event.currentTarget.href;
+
+                iframe.src = url;
+
+                iframe.addEventListener('load', function() {
+                    const style = window.getComputedStyle(document.body);
+                    const isDarkMode = style.backgroundColor === 'rgb(34, 34, 34)';
+
+                    if (isDarkMode) {
+                        // Custom Dark Mode styles for the iframe
+                        const darkModeStyles = `
+                    body {
+                        background-color: #222;
+                        color: #fff;
+                    }
+                `;
+
+                        const darkModeStyleElement = document.createElement('style');
+                        darkModeStyleElement.innerHTML = darkModeStyles;
+                        iframe.contentWindow.document.head.appendChild(darkModeStyleElement);
+                    }
+
+                    // Custom ad-blocker event
+                    /*iframe.contentWindow.document.addEventListener('ad-blocker', function() {
+                        console.log('Ad-blocker event triggered');
+                        // Implement custom ad-blocking logic here
+                    });*/
+                });
+            });
+        });
+    </script>
+
 </x-app-layout>
