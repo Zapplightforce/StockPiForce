d7ed6e0f1cb8a1ac66cd80dcc8b8d06a2b4a6738
Zapplightforce <pers0010@hz.nl>
2023-06-25 16:00:03 +0200

    testing complete

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -1,19 +1,58 @@
+<?php
+
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-use RefreshDatabase;
+    use RefreshDatabase;
 
-public function test_login_and_view_dashboard()
-{
-// Code for User Story 1 system test goes here
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutMiddleware(
+            \App\Http\Middleware\VerifyCsrfToken::class
+        );
+    }
+
+    public function test_login_and_view_dashboard()
+    {
+        // Create a user
+        $user = User::factory()->create([
+            'password' => bcrypt($password = 'password'), // Hash the password
+        ]);
+
+        // Call the login function
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        // Check if the user is redirected to the dashboard
+        $response->assertStatus(302); // 302 is the status code for redirection
+
+        // Check if the user is authenticated
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_update_profile()
+    {
+        // Create a user
+        $user = User::factory()->create();
+
+        // Login as the user
+        $this->actingAs($user);
+
+        // Update user profile
+        $response = $this->patch('/profile', [
+            'name' => 'New Name',
+        ]);
+
+        // Refresh user instance and check if the name has been updated
+        $response->assertRedirect('/profile');
+    }
 }
 
-public function test_update_profile()
-{
-// Code for User Story 2 system test goes here
-}
-}
--- tests/Unit/ProfileControllerTest.php
+++ tests/Unit/ProfileControllerTest.php
@@ -1,1 +1,44 @@
 <?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ProfileControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+public function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutMiddleware(
+            \App\Http\Middleware\VerifyCsrfToken::class
+        );
+    }
+
+    public function test_update_function()
+    {
+        // Create a user
+        $user = User::factory()->create();
+
+        // Login the user
+        $this->actingAs($user);
+
+        // Update user profile
+        $response = $this->patch('/profile', [
+            'name' => 'New Name',
+        ]);
+
+        // Check if the update was successful
+        $response->assertStatus(302);
+
+        // Refresh user instance and check if the name has been updated
+        $user = $user->fresh();
+
+        $this->assertEquals('New Name', $user->name);
+    }
+}
+
+
