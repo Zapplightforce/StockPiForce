669a9b94fa34dfde0f2dc433e6dfdfa02cebc6f7
Zapplightforce <pers0010@hz.nl>
2023-06-25 15:07:18 +0200

    file creation

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,19 @@
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+use RefreshDatabase;
+
+public function test_login_and_view_dashboard()
+{
+// Code for User Story 1 system test goes here
+}
+
+public function test_update_profile()
+{
+// Code for User Story 2 system test goes here
+}
+}
